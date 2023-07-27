b5c95209398381f3f77d850c95230bece35490eb
Sohag Hasan <mailsohaghasan@gmail.com>
2023-07-27 10:54:19 +0600

    frist branch commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-8xl text-center text-red-700 py-10">This Is Next Js Project</h1>
+      <h1 className="text-8xl text-center text-red-700 py-10">Frist Title</h1>
     </div>
   )
 }
