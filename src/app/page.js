8ab9857fbe295735d62f1692f9f40f78eb3b1d26
Sohag Hasan <mailsohaghasan@gmail.com>
2023-07-27 11:09:53 +0600

    four commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div>
       <h1 className="text-8xl text-center text-red-700 py-10">This Is Next Js Project</h1>
       <h1 className="text-8xl text-center text-red-700 py-10">frist Title</h1>
+      <h1 className="text-8xl text-center text-red-700 py-10">Four  Title</h1>
     </div>
   )
 }
